2e09fb336ef27868f0cf7da431ca37b0c270fe40
ubuntu <ubuntu@ip-10-0-1-17>
2025-01-10 15:41:55 +0000

    waleko__gemini-pro-1.5_8afe__id_188

--- tools/accuracy_checker/openvino/tools/accuracy_checker/annotation_converters/amazon.py
+++ tools/accuracy_checker/openvino/tools/accuracy_checker/annotation_converters/amazon.py
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
+import io
 import pickle  # nosec B403  # disable import-pickle check
 from pathlib import Path
 import numpy as np
@@ -34,7 +35,7 @@ class DataIterator:
                  reviews_info,
                  batch_size=128,
                  maxlen=100):
-
+        source = str(source)
         self.source = open(source, 'r', encoding='UTF-8') # pylint: disable=R1732
         self.source_dicts = []
         for source_dict in [uid_voc, mid_voc, cat_voc]:
