7b774949c30abd123a9c6d0e3b8fbd7ee1656e21
ubuntu <ubuntu@ip-10-0-1-122>
2025-01-28 14:16:00 +0000

    waleko__claude-3.5-sonnet_3b54__id_188

--- tools/accuracy_checker/openvino/tools/accuracy_checker/annotation_converters/amazon.py
+++ tools/accuracy_checker/openvino/tools/accuracy_checker/annotation_converters/amazon.py
@@ -29,10 +29,10 @@ class DataIterator:
     def __init__(self, source,
                  uid_voc,
                  mid_voc,
-                 cat_voc,
-                 item_info,
+                 cat_voc, 
+                 item_info, 
                  reviews_info,
-                 batch_size=128,
+                 batch_size=128, 
                  maxlen=100):
 
         self.source = open(source, 'r', encoding='UTF-8') # pylint: disable=R1732
